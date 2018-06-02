83d668102875b7d59f9932ad326ade14dfb2b466
andisatvik <satvikandi@gmail.com>
2018-06-02 17:42:32 -0400

    Message Delivered

--- rename_files.py
+++ rename_files.py
@@ -2,10 +2,13 @@ import os
 import re
 
 def rename_files():
-
+    
+    curr_dir = os.getcwd();
     file_list=os.listdir(r"E:\Courses\Programming Foundations - Python\Photos")
+    os.chdir("E:\Courses\Programming Foundations - Python\Photos");
 
     for file_name in file_list:
         os.rename(file_name,re.sub("^[0-9]+","",file_name))
-        print(file_name)
+
+    os.chdir(curr_dir)
 rename_files()
