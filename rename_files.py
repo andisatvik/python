d14fd5735a5ec4d0e5f8d7e6705840942e8b32d5
andisatvik <satvikandi@gmail.com>
2018-06-02 17:35:22 -0400

    Removing numbers from every file name

--- rename_files.py
+++ rename_files.py
@@ -1,10 +1,11 @@
 import os
+import re
 
 def rename_files():
 
     file_list=os.listdir(r"E:\Courses\Programming Foundations - Python\Photos")
 
     for file_name in file_list:
+        os.rename(file_name,re.sub("^[0-9]+","",file_name))
         print(file_name)
-
 rename_files()
