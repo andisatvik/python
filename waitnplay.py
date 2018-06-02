7cc56ec8553b8196435e69e1d00bd88a5bea9b7c
andisatvik <satvikandi@gmail.com>
2018-06-02 15:05:31 -0400

    while loop with 3 repititions

--- waitnplay.py
+++ waitnplay.py
@@ -1,6 +1,11 @@
 import webbrowser
 import time
 
+loop_var = 1
+loop_end = 3
 
-time.sleep(3)
-webbrowser.open("https://youtu.be/LpGACuMgtNA")
+while loop_var <= loop_end:
+
+    time.sleep(3)
+    webbrowser.open("https://youtu.be/LpGACuMgtNA")
+    loop_var+=1
