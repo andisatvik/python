4da6e719dd44e109def16336c7f53b49420c36f2
andisatvik <satvikandi@gmail.com>
2018-06-05 19:35:06 -0400

    Display the earlier message multiple times in different lines

--- user_input.py
+++ user_input.py
@@ -5,7 +5,8 @@ import datetime
 curr_year = datetime.date.today().year
 name = input("Please enter name : ")
 age = input("Please enter age : ")
+num = int(input("How many number of times do you want the message to be displayed ?"))
 
 #print(curr_year - int(age) + 100)
 
-print(name,", Your age will be 100 in the year: ",(curr_year - int(age) +100))
+print(num*(name + ", Your age will be 100 in the year: " + str(curr_year - int(age) +100)+"\n"))
