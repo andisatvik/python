71fd4f73b22be5e4f58f964b46b821068ac24ee4
andisatvik <satvikandi@gmail.com>
2018-06-06 23:10:47 -0400

    Binary Search

---
+++ bin_search.py
@@ -0,0 +1,19 @@
+# Binary search to check if an element is present in a list
+
+import random
+
+num_list = random.sample(range(20),7)
+
+num = int(input("Enter the number you are searching for: "))
+
+def num_pres(num_list, num):
+
+    #sorting to prepare for binary search
+    num_list.sort()
+    if (num < num_list[int(len(num_list)/2)]):
+        num_list = 
+
+    if :
+        print(str(num)+" present in list")
+    else:
+        print(str(num)+" present in list")
--- url_read.py
+++ url_read.py
@@ -1,1 +1,15 @@
-# Import 
+# Print all the headings from New York times
+
+import requests
+from bs4 import BeautifulSoup 
+
+url = "https://www.nytimes.com/"
+
+r = requests.get(url)
+
+r_html = r.text
+
+soup = BeautifulSoup(r_html, 'html.parser')
+
+title = soup.find('div','title')
+print(title)
